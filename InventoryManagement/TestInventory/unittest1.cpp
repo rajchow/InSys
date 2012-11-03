aa4f8f116717a527cbaacb4045f5c996895b3f76
Michael Rowland <michaelr87@gmail.com>
2012-11-03 13:55:15 -0600

    Project with included TestInventory

---
+++ InventoryManagement/TestInventory/unittest1.cpp
@@ -0,0 +1,19 @@
+#include "stdafx.h"
+#include "CppUnitTest.h"
+#include "../InventoryManagement/TableInterface.h"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace TestInventory
+{		
+	TEST_CLASS(UnitTest1)
+	{
+	public:
+		
+		TEST_METHOD(TestMethod1)
+		{
+			
+		}
+
+	};
+}
